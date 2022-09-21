5930d4330a3db038457d950d4b2bc60acc92e60b
anshul bansal <akbt.tech17@gmail.com>
2022-09-21 20:16:22 +0530

    Created DB Context executing Scaffold command scaffold-dbcontext "server=.\sqlexpress; database=BookMyMovie; trusted_connection=true;" Microsoft.EntityFrameworkCore.SqlServer -o Models -t Movie

---
+++ BookMyMovie_Angular_Backend/Models/BookMyMovieContext.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace BookMyMovie_Angular_Backend.Models
+{
+    public partial class BookMyMovieContext : DbContext
+    {
+        public BookMyMovieContext()
+        {
+        }
+
+        public BookMyMovieContext(DbContextOptions<BookMyMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Movie> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=.\\sqlexpress; database=BookMyMovie; trusted_connection=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Movie>(entity =>
+            {
+                entity.ToTable("Movie");
+
+                entity.Property(e => e.MovieId).HasColumnName("movieId");
+
+                entity.Property(e => e.AgeRating)
+                    .HasMaxLength(4)
+                    .IsUnicode(false)
+                    .HasColumnName("ageRating");
+
+                entity.Property(e => e.CostPerSeat)
+                    .HasColumnName("costPerSeat")
+                    .HasDefaultValueSql("((250))");
+
+                entity.Property(e => e.Duration)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("duration");
+
+                entity.Property(e => e.Genres)
+                    .HasMaxLength(200)
+                    .IsUnicode(false)
+                    .HasColumnName("genres");
+
+                entity.Property(e => e.ImageUrl)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("imageUrl");
+
+                entity.Property(e => e.Language)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("language");
+
+                entity.Property(e => e.MovieName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("movieName");
+
+                entity.Property(e => e.MovieType)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("movieType");
+
+                entity.Property(e => e.Ratings).HasColumnName("ratings");
+
+                entity.Property(e => e.ReleaseDate)
+                    .HasColumnType("datetime")
+                    .HasColumnName("releaseDate");
+
+                entity.Property(e => e.ShowTime)
+                    .HasColumnType("datetime")
+                    .HasColumnName("showTime");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ BookMyMovie_Angular_Backend/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BookMyMovie_Angular_Backend.Models
+{
+    public partial class Movie
+    {
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public int? Ratings { get; set; }
+        public string Genres { get; set; }
+        public string ImageUrl { get; set; }
+        public int? CostPerSeat { get; set; }
+        public DateTime? ShowTime { get; set; }
+        public string Duration { get; set; }
+        public string AgeRating { get; set; }
+        public string Language { get; set; }
+        public string MovieType { get; set; }
+    }
+}
