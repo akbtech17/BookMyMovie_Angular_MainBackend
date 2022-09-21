9f9a9f2bc29df0b34297a1ad75e865c08c787f03
anshul bansal <akbt.tech17@gmail.com>
2022-09-21 22:33:38 +0530

    added cors service to communicate with frontend

--- BookMyMovie_Angular_Backend/Startup.cs
+++ BookMyMovie_Angular_Backend/Startup.cs
@@ -28,6 +28,7 @@ namespace BookMyMovie_Angular_Backend
         {
             services.AddControllers();
             services.AddDbContext<BookMyMovieContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("mycon")));
+            services.AddCors();                             //Add CORS ---- Cross Origin Resourse Shard
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,7 +40,7 @@ namespace BookMyMovie_Angular_Backend
             }
 
             app.UseRouting();
-
+            app.UseCors(options => { options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin(); });
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
