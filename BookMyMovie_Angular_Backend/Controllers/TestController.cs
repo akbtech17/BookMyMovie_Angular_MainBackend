fc8dd1f64d88b3c623a281f1eb1ee3e39ef28650
anshul bansal <akbt.tech17@gmail.com>
2022-09-21 20:12:58 +0530

    Created Test Controller and set it to be a default route

---
+++ BookMyMovie_Angular_Backend/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookMyMovie_Angular_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // GET: api/<TestController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hi Anshul Here!");
+        }
+    }
+}
