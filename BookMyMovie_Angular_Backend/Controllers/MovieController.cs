34421b6e5e8ebdd202525c469dd0f563ebe941a2
anshul bansal <akbt.tech17@gmail.com>
2022-10-01 00:48:26 +0530

    delete movie api is working

--- BookMyMovie_Angular_Backend/Controllers/MovieController.cs
+++ BookMyMovie_Angular_Backend/Controllers/MovieController.cs
@@ -40,5 +40,23 @@ namespace BookMyMovie_Angular_Backend.Controllers
                 return BadRequest("Error " + ex.InnerException.Message);
             }
         }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult DeleteMovie(int id) {
+            try
+            {
+                var data = db.Movies.Where(m => m.MovieId == id).FirstOrDefault();
+                if (data != null)
+                {
+                    db.Movies.Remove(data);
+                    db.SaveChanges();
+                }
+                return Ok();
+            }
+            catch (Exception ex) {
+                return BadRequest("Error " + ex.InnerException.Message);
+            }
+        }
     }
 }
