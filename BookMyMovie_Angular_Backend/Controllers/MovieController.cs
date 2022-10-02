79e0760a53414d3208c492935dca29356c2133d0
anshul bansal <akbt.tech17@gmail.com>
2022-10-03 01:05:42 +0530

    built the edit movie api

--- BookMyMovie_Angular_Backend/Controllers/MovieController.cs
+++ BookMyMovie_Angular_Backend/Controllers/MovieController.cs
@@ -75,5 +75,34 @@ namespace BookMyMovie_Angular_Backend.Controllers
                 return BadRequest("Error " + ex.InnerException.Message);
             }
         }
+
+        [HttpPut]
+        public IActionResult UpdateMovie(Movie new_data)
+        {
+            try
+            {
+                var old_data = db.Movies.Where(m => m.MovieId == new_data.MovieId).FirstOrDefault();
+                if (old_data != null)
+                {   
+                    old_data.MovieName = new_data.MovieName;
+                    old_data.ReleaseDate = new_data.ReleaseDate; 
+                    old_data.Ratings = new_data.Ratings; 
+                    old_data.Genres = new_data.Genres; 
+                    old_data.ImageUrl = new_data.ImageUrl; 
+                    old_data.CostPerSeat = new_data.CostPerSeat; 
+                    old_data.ShowTime = new_data.ShowTime;
+                    old_data.Duration = new_data.Duration;
+                    old_data.AgeRating = new_data.AgeRating;
+                    old_data.Language = new_data.Language;
+                    old_data.MovieType = new_data.MovieType;
+                }
+                db.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error " + ex.InnerException.Message);
+            }
+        }
     }
 }
