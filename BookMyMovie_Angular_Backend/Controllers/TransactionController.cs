57aee53378378a74ab1e0c26b6671178b43cf6a6
anshul bansal <akbt.tech17@gmail.com>
2022-10-08 19:11:16 +0530

    posting the message to azure queue on successful transaction

---
+++ BookMyMovie_Angular_Backend/Controllers/TransactionController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+
+namespace BookMyMovie_Angular_Backend.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TransactionController : ControllerBase
+	{
+		public static string connAzureStorage = "DefaultEndpointsProtocol=https;AccountName=anshulkumarstorage256;AccountKey=PEdBR+R7xlLxZdCrfCmURzVlWLYEUlpmkGdGJP9diqkcyN9uIfxB9ZHnx3L481ULKBYnOMznHlgA+AStix+Sjg==;EndpointSuffix=core.windows.net";
+
+		[HttpPost]
+		[Route("")]
+		public IActionResult CreateTransaction()
+		{
+			try
+			{
+				string message = "Hi, Anshul first message to queue";
+				AddMessageToQueue(message);
+			}
+			catch (Exception ex) 
+			{
+				return BadRequest(ex.InnerException.Message);
+			}
+			return Ok();
+		}
+
+		public static void AddMessageToQueue(string message) 
+		{
+			CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connAzureStorage);
+			CloudQueueClient cloudQueueClient = storageAccount.CreateCloudQueueClient();
+			CloudQueue cloudQueue = cloudQueueClient.GetQueueReference("myqueue");
+			CloudQueueMessage queueMessage = new CloudQueueMessage(message);
+			cloudQueue.AddMessageAsync(queueMessage);
+		}
+	}
+}
